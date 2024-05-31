b4aeec21b8e9ae9316f34c16d34b93235c852ffa
YojiUtsumi <cocosuke.pc.one@gmail.com>
2024-05-30 22:54:59 -0700

    make mangement top page and access auth list page

--- program/management/urls.py
+++ program/management/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'management'
 
 urlpatterns = [
+    path('', views.ManagementTopPageView.as_view(), name='top_page'),
+
     path('user_access/create/', views.CreateUserAccessAuthorizationView.as_view(), name='user_access-create'),
     path('user_access/', views.ListUserAccessAuthorizationsView.as_view(), name='user_access'),
 
--- program/management/views.py
+++ program/management/views.py
@@ -1,17 +1,16 @@
 from django.views import generic
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . import models, forms
 from user import models as user_models
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
 
 
 class CheckAccessAuthorization(LoginRequiredMixin):
     field_required = None
     field_not_required = None
-    redirect_to = reverse_lazy('user:login')
+    redirect_to = reverse_lazy('management:top_page')
 
     def dispatch(self, request, *args, **kwargs):
         # make sure user already logined or not
@@ -52,25 +51,66 @@ class CheckAccessAuthorization(LoginRequiredMixin):
         return redirect(self.redirect_to)
 
 
+class ManagementTopPageView(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'management/top_page.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # make sure user already logined or not
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+
+        # get user
+        user = self.request.user
+
+        # if user is staff, it can access
+        if user.is_staff:
+            return super().dispatch(request, *args, **kwargs)
+
+        # make sure user has authorization
+        query = models.UserAccessAuthorization.objects.filter(
+            user_object=user, url__contains='/management/'
+        )
+        if query:
+            return super().dispatch(request, *args, **kwargs)
+
+        # make sure user has employee information
+        if not hasattr(user, 'employeeinformation'):
+            return redirect(reverse_lazy('common:top_page'))
+        
+        # make sure user has type
+        if not user.employeeinformation.employee_type_object:
+            return redirect(reverse_lazy('common:top_page'))
+
+        # make type of user has authorization
+        type = self.request.user.employeeinformation.employee_type_object
+        query = models.EmployeeTypeAccessAuthorization.objects.filter(
+            employee_type_object=type, url__contains='/management/'
+        )
+        if query:
+            return super().dispatch(request, *args, **kwargs)
+        
+        return redirect(reverse_lazy('common:top_page'))
+
+
 class CreateUserAccessAuthorizationView(CheckAccessAuthorization, generic.CreateView):
     model = models.UserAccessAuthorization
     form_class = forms.UserAccessAuthorizationsForm
-    template_name = 'management/create.html'
-    success_url = reverse_lazy()
+    template_name = 'management/access_authorization_create.html'
+    success_url = reverse_lazy('management:user_access')
 
 
 class CreateEmployeeTypeAccessAuthorizationView(CheckAccessAuthorization, generic.CreateView):
     model = models.EmployeeTypeAccessAuthorization
     form_class = forms.EmployeeTypeAccessAuthorizationsForm
-    template_name = 'management/create.html'
-    success_url = reverse_lazy()
+    template_name = 'management/access_authorization_create.html'
+    success_url = reverse_lazy('management:employeetype_access')
 
 
 class ListUserAccessAuthorizationsView(generic.ListView):
     model = models.UserAccessAuthorization
-    template_name = ''
+    template_name = 'management/user_access_authorization.html'
 
 
 class ListEmployeeTypeAccessAuthorizationView(generic.ListView):
     model = models.EmployeeTypeAccessAuthorization
-    template_name = ''
+    template_name = 'management/employee_type_access_authorization.html'
