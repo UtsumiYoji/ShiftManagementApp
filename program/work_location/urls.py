4479ed20ee8857d8ce7fb16e6d8d9014be360c19
YojiUtsumi <cocosuke.pc.one@gmail.com>
2024-08-01 11:48:14 -0700

    added additional user data, fixed urls from workplace, added gitignore

--- program/work_location/urls.py
+++ program/work_location/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'management'
+app_name = 'work_location'
 
 urlpatterns = [
     path('', views.ListWorkLocationView.as_view(), name='work_location-list'),
 
     # User access authorization
-    path('work_location/create/', views.CreateWorkLocationView.as_view(), name='work_location-create'),
-    path('work_location/<int:pk>', views.UpdateWorkLocationView.as_view(), name='work_location-update'),
-    path('work_location/<int:pk>/businesshour', views.BusinnessHourView.as_view(), name='work_location_businesshour'),
-    path('work_location/<int:pk>/delete', views.DeleteWorkLocationView.as_view(), name='work_location-delete'),
+    path('create/', views.CreateWorkLocationView.as_view(), name='work_location-create'),
+    path('<int:pk>', views.UpdateWorkLocationView.as_view(), name='work_location-update'),
+    path('<int:pk>/businesshour', views.BusinnessHourView.as_view(), name='work_location_businesshour'),
+    path('<int:pk>/delete', views.DeleteWorkLocationView.as_view(), name='work_location-delete'),
 
 ]
