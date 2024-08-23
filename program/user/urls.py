f479926069b46249a6220f6df12f749df5fc8551
YojiUtsumi <cocosuke.pc.one@gmail.com>
2024-08-23 09:08:53 -0700

    change design of user

--- program/user/urls.py
+++ program/user/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='log-in'),
     path('logout/', LogoutView.as_view(), name='log-out'),
 
-    path('', views.UpdateUserView.as_view(), name='user-update'),
+    path('', views.UpdateUserView.as_view(), name='user'),
     path('password/', views.PasswordChangeView.as_view(), name='password'),
     # path('delete/', views.DeleteUserView.as_view(), name='user-delete'),
 
-    path('address/', views.AddressView.as_view(), name='address-update'),
+    path('address/', views.AddressView.as_view(), name='address'),
 
-    path('bank_information/', views.BankInformationView.as_view(), name='bank_information-update'),
+    path('bank_information/', views.BankInformationView.as_view(), name='bank_information'),
 ]
