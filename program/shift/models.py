acbaf4f0a6b3c843dfea343b4222a4729a964a0f
YojiUtsumi <cocosuke.pc.one@gmail.com>
2024-09-27 14:23:57 -0700

    delete note, fix z-index of header

--- program/shift/models.py
+++ program/shift/models.py
@@ -8,7 +8,7 @@ class UserShift(models.Model):
     work_location_object = models.ForeignKey(WorkLocation, on_delete=models.CASCADE, null=False, blank=False)
     start_at = models.DateTimeField(null=False, blank=False)
     finish_at = models.DateTimeField(null=False, blank=False)
-    note = models.CharField(max_length=100, null=True, blank=True)
+
 
 class BreakTime(models.Model):
     user_object = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=False, blank=False)
